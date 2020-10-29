92fec37c80434b840a4bf0194d1f728bc8542a99
NicolasCIBULKA <nico.cibulka@gmail.com>
2020-10-29 14:54:26 +0100

    suite pas accueil

--- main.php
+++ main.php
@@ -24,9 +24,20 @@
 			</li>
 		</ul>		
 	</nav>
+	<div class="container-fluid">
+		<h2>Information générale sur vos comptes</h2>
 
-	<h2>Information générale sur vos comptes</h2>
+		<p>28/10/2020</p>
+		<div class="flex-row align-items-center">
+			
+			<h3>Compte Courant</h3>
 
+			<p>234.45€</p>
+			<p>Montant actuel sur le compte courant</p>
+		</div>
+		
+
+	</div>
 	
 
 	<?php include("templates/footer.inc.php"); ?>
